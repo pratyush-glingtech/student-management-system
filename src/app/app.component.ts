1b3a95c9e00507c40153f24d6ebb008ac0e1a1e9
pratyush <pratyush.singh@glingtech.onmicrosoft.com>
2024-12-18 09:03:55 +0530

    started working on register page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from "./components/login/login.component";
+import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  imports: [RouterOutlet, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
